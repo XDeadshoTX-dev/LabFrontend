9af3158992dbf48895a754a184e8a37aa88f1c17
Denys <svinarchuk.den@gmail.com>
2025-02-22 22:12:39 +0200

    Added translate support for C

--- backend/schemas/translate/CTranslateSchema.cs
+++ backend/schemas/translate/CTranslateSchema.cs
@@ -14,11 +14,25 @@ namespace WpfApp2.backend.schemas.translate
         public CTranslateSchema(int deep, AbstractBlock block)
         {
             this.deepSchema = deep;
-            pattern = @"(static\s+void\s+Main\s*\(.*?\)\s*\{)(.*?)(\})";
+            this.block = block;
+
+            string amountSpaces = this.block.GetIndent(this.deepSchema);
+            pattern = $@"({amountSpaces}\{{)(.*?)(\n{amountSpaces}\}})";
         }
         public override string InsertCode(Match match, string code)
         {
-            return string.Empty;
+            string openingBrace = match.Groups[1].Value.Trim();  // {
+            string content = match.Groups[2].Value.Trim();       // raw content
+            string closingBrace = match.Groups[3].Value.Trim();  // }
+
+            string newContent = string.IsNullOrEmpty(content)
+                ? $"\n{block.GetIndentCode(this.deepSchema + 1)}\n"
+                : $"\n{block.GetIndent(this.deepSchema + 1)}{content}\n{block.GetIndentCode(this.deepSchema + 1)}\n";
+
+            return $"{block.GetIndent(this.deepSchema)}" +
+                   $"{openingBrace}" +
+                   $"{newContent}" +
+                   $"{block.GetIndent(this.deepSchema)}{closingBrace}";
         }
     }
 }
