2b4635892059cd48131a61d4a5a3ca2dc7835095
Denys <svinarchuk.den@gmail.com>
2025-02-23 11:58:53 +0200

    read rawContent

--- backend/schemas/translate/JavaTranslateSchema.cs
+++ backend/schemas/translate/JavaTranslateSchema.cs
@@ -17,26 +17,20 @@ namespace WpfApp2.backend.schemas.translate
             this.block = block;
 
             int currentBrace = this.deepSchema + 1;
-            //pattern = $@"^(?<line>(?:[^\{{]*\{{){{{currentBrace}}})(?<content>[\s\S]*)$";
-            pattern = $@"^(?<line>(?:[^\{{]*\{{){{{currentBrace}}})(?<content>[\s\S]*?)(\}})";
+            //pattern = $@"^(?<line>(?:[^\{{]*\{{){{{currentBrace}}})(?<content>[\s\S]*?)(\}})";
+            int amountSpaces = 4;
+            pattern = $@"^\s{{{amountSpaces}}}.*\{{\s*\n(?<rawContent>[\s\S]*?)^\s{{{amountSpaces}}}\}}";
         }
         public override string InsertCode(Match match, string fileContent, string code)
         {
-            // Извлекаем строку (или блок), в котором найдено нужное вхождение фигурных скобок.
-            string targetLine = match.Groups["line"].Value;
-            string contentAfterBrace = match.Groups["content"].Value.Trim();
+            // Извлекаем контент между скобками
+            string rawContent = match.Groups["rawContent"].Value;
 
-            // Добавляем новый код после открывающей скобки с правильным отступом
-            string modifiedContent = string.IsNullOrEmpty(contentAfterBrace) 
-                                    ?  targetLine + "\n" +
-                                       contentAfterBrace +
-                                       $"{this.block.GetIndentCode(this.deepSchema + 1)}" + "\n" +
-                                       $"{this.block.GetIndent(this.deepSchema)}}}"
-                                    : targetLine + "\n" +
-                                       $"{this.block.GetIndent(this.deepSchema + 1)}{contentAfterBrace}" +
-                                       $"\n{this.block.GetIndentCode(this.deepSchema + 1)}" + 
-                                       $"\n{this.block.GetIndent(this.deepSchema)}}}";
+            // Добавляем новый код перед закрывающей скобкой
+            string modifiedContent = rawContent + "\n" +
+                                     $"{this.block.GetIndentCode(this.deepSchema + 1)}";
 
+            // Вставляем обратно в файл
             return modifiedContent;
         }
     }
