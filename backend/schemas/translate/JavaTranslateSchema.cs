fd7ba310261d8b62af72b8aa1850c268a9e18b23
Denys <svinarchuk.den@gmail.com>
2025-02-23 12:21:47 +0200

    Added translate support for Java

--- backend/schemas/translate/JavaTranslateSchema.cs
+++ backend/schemas/translate/JavaTranslateSchema.cs
@@ -17,20 +17,25 @@ namespace WpfApp2.backend.schemas.translate
             this.block = block;
 
             int currentBrace = this.deepSchema + 1;
-            //pattern = $@"^(?<line>(?:[^\{{]*\{{){{{currentBrace}}})(?<content>[\s\S]*?)(\}})";
-            int amountSpaces = 4;
-            pattern = $@"^\s{{{amountSpaces}}}.*\{{\s*\n(?<rawContent>[\s\S]*?)^\s{{{amountSpaces}}}\}}";
+            int amountSpaces = 4 * this.deepSchema;
+            pattern = $@"^\s{{{amountSpaces}}}(?<before>[^\{{]*)\{{\s*\n(?<rawContent>[\s\S]*?)^\s{{{amountSpaces}}}\}}";
         }
         public override string InsertCode(Match match, string fileContent, string code)
         {
-            // Извлекаем контент между скобками
+            string beforeBrace = match.Groups["before"].Value;
+            string openingBrace = "{";
             string rawContent = match.Groups["rawContent"].Value;
+            string closingBrace = "}";
 
-            // Добавляем новый код перед закрывающей скобкой
-            string modifiedContent = rawContent + "\n" +
-                                     $"{this.block.GetIndentCode(this.deepSchema + 1)}";
+            string modifiedContent = string.IsNullOrEmpty(rawContent)
+                                     ? $"{this.block.GetIndent(this.deepSchema)}" + beforeBrace + openingBrace + "\n" +
+                                       $"{this.block.GetIndentCode(this.deepSchema + 1)}" + "\n" +
+                                       this.block.GetIndent(this.deepSchema) + closingBrace
+                                     : $"{this.block.GetIndent(this.deepSchema)}" + beforeBrace + openingBrace + "\n" +
+                                       rawContent +
+                                       $"{this.block.GetIndentCode(this.deepSchema + 1)}" + "\n" +
+                                       this.block.GetIndent(this.deepSchema) + closingBrace;
 
-            // Вставляем обратно в файл
             return modifiedContent;
         }
     }
