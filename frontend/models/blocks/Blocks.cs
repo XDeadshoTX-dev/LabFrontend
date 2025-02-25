78fadf68ee695e99989ecea3428924169a999bd7
Denys <svinarchuk.den@gmail.com>
2025-02-25 10:28:49 +0200

    Cleanup

--- frontend/models/blocks/Blocks.cs
+++ frontend/models/blocks/Blocks.cs
@@ -31,14 +31,4 @@ namespace WpfApp2.frontend.blocks
             };
         }
     }
-    //public class Block
-    //{
-    //    public int Id { get; set; }
-    //    public string Type { get; set; }
-    //    public string Text { get; set; }
-    //    public int? NextBlockId { get; set; }
-    //    public int? TrueBlockId { get; set; }
-    //    public int? FalseBlockId { get; set; }
-    //    public Point Position { get; set; }
-    //}
 }
